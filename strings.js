1c3fd30cc2e9c2c0f03e36a8308e368df94cd7ea
rtulnikov <tulnikovratmir@gmail.com>
2025-05-04 19:21:22 +0300

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,16 @@
+let ass = "Дима кака-ляля и балбес";
+let rat = "Рат";
+let mir = "мир";
+let two = 2;
+
+console.log(rat + mir, ass.slice(5));
+
+console.log(ass.slice(17));
+
+console.log(ass.slice(0,4).toLowerCase());
+
+console.log(ass.slice(0,4).toLowerCase().length );
+
+let string = "я каКая-То кРиваЯ стрОка";
+//Я какая-то кривая строка
+
