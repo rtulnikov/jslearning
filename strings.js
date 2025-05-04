d5aef3fc68643c686479396d4ddc9d201c7dc956
rtulnikov <tulnikovratmir@gmail.com>
2025-05-04 19:21:54 +0300

    Update strings.js

--- strings.js
+++ strings.js
@@ -4,11 +4,8 @@ let mir = "мир";
 let two = 2;
 
 console.log(rat + mir, ass.slice(5));
-
 console.log(ass.slice(17));
-
 console.log(ass.slice(0,4).toLowerCase());
-
 console.log(ass.slice(0,4).toLowerCase().length );
 
 let string = "я каКая-То кРиваЯ стрОка";
