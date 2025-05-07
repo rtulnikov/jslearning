3cd81fd007e60cb8e9659d6474dd9af01be158c3
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-05-07 15:42:33 +0300

    bool

--- boolean.js
+++ boolean.js
@@ -1,13 +1,22 @@
-let iLoveJs = true;
-let isDestroyed = false;
-let haveBullet = true;
-let haveHP = false;
+let bullets = 0;
+let hitPoints = 2750;
 
-const canShoot = haveBullet && haveHP; 
-const canPlay = (haveBullet || haveHP) && !isDestroyed;
+
+let iLoveJs = true;
+let haveHP = hitPoints > 0;
+let haveBullets = bullets > 0;
+let isDestroyed = !haveHP;
+const canShoot = haveBullets && haveHP;
+
+const canPlay = (haveBullets || haveHP) && !isDestroyed;
 // const canPlay = !isDestroyed;
 let isRaining = true;
 
 
-console.log(canPlay);
+console.log(isDestroyed);
 console.log(canShoot);
+
+let age = 10;
+let needAge = 8;
+const climb = needAge < age;
+console.log(climb);
