fcc393688d5df6f5b185aaace8e05213604e5be1
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-05-09 11:58:53 +0300

    alert

--- boolean.js
+++ boolean.js
@@ -2,10 +2,9 @@ let bullets = 0;
 let hitPoints = 2750;
 
 
-let iLoveJs = true;
-let haveHP = hitPoints > 0;
-let haveBullets = bullets > 0;
-let isDestroyed = !haveHP;
+const haveHP = hitPoints > 0;
+const haveBullets = bullets > 0;
+const isDestroyed = !haveHP;
 const canShoot = haveBullets && haveHP;
 
 const canPlay = (haveBullets || haveHP) && !isDestroyed;
@@ -16,7 +15,8 @@ let isRaining = true;
 console.log(isDestroyed);
 console.log(canShoot);
 
-let age = 10;
+let age = 7;
 let needAge = 8;
-const climb = needAge < age;
+const climb = needAge <= age;
+
 console.log(climb);
