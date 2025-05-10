e40c2b17270c88eb75b657eedb9143d71b473e33
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-05-10 11:58:11 +0300

    шаблонные строки

--- alert.js
+++ alert.js
@@ -4,10 +4,18 @@
 // let needAge = 10;
 // const jump = age <= age;
 // const jf = prompt()
+// let age = prompt('Введите ваш возраст');
+// console.log(age);
+
+
+console.log('Ваш возраст 10 '+ age);
+
 let age = prompt('Введите ваш возраст');
-console.log(age);
+console.log( 'Ваш возраст ' + age + ' лет');
+console.log( `Ваш возраст ${age} лет`);
+
 const needAge = 12;
-const shoot =  age >= needAge;
+const shoot =  age === needAge;
 console.log(shoot);
 
-// console.log('Ваш возраст 10 '+ age);
+console.log( 'undefined' === undefined );
