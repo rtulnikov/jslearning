2d02ba21cc841328175dd71333e628ee4980ec99
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-05-15 19:17:51 +0300

    prompt

--- alert.js
+++ alert.js
@@ -12,6 +12,7 @@ console.log('Ваш возраст 10 '+ age);
 
 let age = prompt('Введите ваш возраст');
 console.log( 'Ваш возраст ' + age + ' лет');
+
 console.log( `Ваш возраст ${age} лет`);
 
 const needAge = 12;
