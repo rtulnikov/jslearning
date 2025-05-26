d7c8569a9bdf55cec83f6213e1ecab9cd67de4eb
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-05-26 20:53:24 +0300

    work update

--- alert.js
+++ alert.js
@@ -8,17 +8,18 @@
 // console.log(age);
 
 
-let age = prompt('Введите ваш возраст');
-console.log( 'Ваш возраст ' + age + ' лет');
+// let age = prompt('Введите ваш возраст');
+// console.log( 'Ваш возраст ' + age + ' лет');
 
-<<<<<<< HEAD
+
+
 console.log( `Ваш возраст ${age} лет`);
-=======
+
 console.log( `Ваш 
     возраст 
     ${age} 
     лет`);
->>>>>>> c919debc5031f017fadb332fb3ac63c3a6c269c4
+
 
 const needAge = 12;
 const shoot =  age === needAge;
