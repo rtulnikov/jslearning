aade0864830f7cbbdc04818f64b5ec78c4146c68
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-06-09 11:32:47 +0300

    sculkindex

--- condition.js
+++ condition.js
@@ -1,19 +1,19 @@
-const bodyParts = ["глаз", "нос", "череп","жопа", "лапки"];
+const bodyParts = [ "глаз",  "нос",  "жопа",  "лапки"];
 const numbers = [4, 3, 2, 1];
 
 
+const sculkIndex = bodyParts.indexOf('череп');
+if (sculkIndex === -1) {
+    console.log('элемент не найден')
+} else {
+    const result = bodyParts.slice(sculkIndex, sculkIndex + 2)
+    console.log(result);
+}
+// const result = bodyParts.slice(sculkIndex, sculkIndex + 2);
+
+console.log(bodyParts.indexOf('череп'));
+console.log(bodyParts['череп']);
+
+console.log(sculkIndex);
 
 
-if (numbers[0] === '4' && numbers[1] === 3){
-    console.log('Привет, Ратмир')
-} else if(bodyParts[1] == 'нос') {
-    console.log('У тебя большой нос')
-} else {
-    console.log('Пока, Ратмир')
-};
-let name = 'Аня';
-if (name.length > 5) {
-    console.log('У вас длинное имя')
-} else {
-    console.log('У вас короткое имя')
-};
