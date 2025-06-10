4bbdd03cff9446c69d0ed693ade6f9c560be252f
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-06-10 10:02:28 +0300

    switch

--- conditions/condition.js
+++ conditions/condition.js
@@ -1,12 +1,12 @@
-const bodyParts = [ "глаз",  "нос",  "жопа",  "лапки"];
+const bodyParts = [ "глаз", "череп", "нос",  "жопа",  "лапки"];
 const numbers = [4, 3, 2, 1];
 
-
-const sculkIndex = bodyParts.indexOf('череп');
+const element = 'лапки'
+const sculkIndex = bodyParts.indexOf(element);
 if (sculkIndex === -1) {
     console.log('элемент не найден')
 } else {
-    const result = bodyParts.slice(sculkIndex, sculkIndex + 2)
+    const result = bodyParts.slice( sculkIndex , sculkIndex + 2 )
     console.log(result);
 }
 // const result = bodyParts.slice(sculkIndex, sculkIndex + 2);
