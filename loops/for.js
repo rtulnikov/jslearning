71dd30431188e2c42aa6e10e54757194389b241d
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-06-16 12:32:40 +0300

    for.js

--- loops/for.js
+++ loops/for.js
@@ -11,8 +11,35 @@
 //     }
 //     console.log(number);
 // } 
+
 let sum = 0;
 for(let i = 1; i <= 11;  i++) {
     sum += i
+// console.log(sum);
+} 
 console.log(sum);
+
+let string = 'Дима какашка и балбес';
+let words = string.split(' ');
+let word =   words[0][0].toUpperCase() + words[0].slice(1);
+let word1 =  words[1][0].toUpperCase() + words[1].slice(1);
+let word2 =  words[2][0].toUpperCase() + words[2].slice(1);
+let word3 =  words[3][0].toUpperCase() + words[3].slice(1);
+
+
+
+let result = [];
+
+
+for(let i = 0; i < words.length; i++) {
+console.log(words[i][0].toUpperCase() + words[i].slice(1));
 }
+for(let elem of words) {
+   result.push( elem[0].toUpperCase() + elem.slice(1));
+}
+console.log(result.join(' '));
+for(let i in words) {
+    // console.log(words[i][0].toUpperCase() + words[i].slice(1))
+console.log(i)
+}
+
