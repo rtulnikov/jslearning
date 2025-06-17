d53e0ac6a05d928ec97d9e3f28b6547fed76a096
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-06-17 18:56:45 +0300

    полтора часа получали случайный элемент массива

--- conditions/ternarnik.js
+++ conditions/ternarnik.js
@@ -23,18 +23,41 @@ if (targetDestroyed) {
 
 targetDestroyed ? console.log('Цель уничтожена') : console.log('Стреляй еще'); 
 
+let ranks = ['рядовой', 'сержант', 'лейтенант'];
+console.log(ranks)
 
-const rank = 'рядовой';
+// const rank = ranks[2];
+let rank = ranks[Math.floor(Math.random() *ranks.length)];
+let randomIndex = ranks[rank];
+console.log(rank);
 
-if (rank === 'рядовой') {
-    let exp = 10; 
-    let damage =  exp > 5 ? 30 : 25;
-    console.log('Выполняйте приказ! Защитите позицию! Ваш урон ' + damage );
-    } else  if (rank === 'сержант') {
-        console.log('Доложите о готовности! Вы ведущий в бой.')
-    } else  if (rank === 'лейтенант') {
-        console.log('Ваша задача — координация! Направляйте подразделения.')
-    } else {
-        console.log('Неизвестное звание. Ждите подтверждения...')
-    };
 
+
+
+// if (rank === 'рядовой') {
+//     let exp = 10; 
+//     let damage =  exp > 5 ? 30 : 25;
+//     console.log('Выполняйте приказ! Защитите позицию! Ваш урон ' + damage );
+//     } else  if (rank === 'сержант') {
+//         console.log('Доложите о готовности! Вы ведущий в бой.')
+//     } else  if (rank === 'лейтенант') {
+//         console.log('Ваша задача — координация! Направляйте подразделения.')
+//     } else {
+//         console.log('Неизвестное звание. Ждите подтверждения...')
+//     };
+
+switch (rank) {
+   case 'рядовой' : 
+    let exp = 10;
+    let damage = exp > 5 ? 30 :25 ;
+    console.log('Выполняйте приказ! Защитите позицию! Ваш урон ' + damage);
+    break
+    case 'сержант' : 
+    console.log('Доложите о готовности! Вы ведущий в бой.')
+    break
+    case 'лейтенант' : 
+    console.log('Ваша задача — координация! Направляйте подразделения.')
+    break
+    default : 
+     console.log('Неизвестное звание. Ждите подтверждения...')
+};
