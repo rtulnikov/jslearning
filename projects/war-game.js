d20e9e121afc7b06554771f989e3f561b221c082
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-06-30 13:11:58 +0300

    defaultArgs.js

--- projects/war-game.js
+++ projects/war-game.js
@@ -1,34 +1,51 @@
-let playerHp = 100;
-let enemyHp = 100;
+const MAX_DAMAGE = 20;
+const CRIT_MULTIPLICATOR = 1.5;
+const HP = 100;
+const CRIT_CHANCE = 0.3;
 
-while(playerHp > 0 && enemyHp > 0) {
-    let shoot = confirm('стрелять?')
-    if(!shoot ) {
-        break
-    }
-    let enemyDamage = Math.ceil(Math.random() *20);
-    let enemyCritDamage = Math.ceil(Math.random() *10);
-    let enemycrit = enemyCritDamage > 7 ? 1.5 : 1;
-    enemyDamage = Math.ceil( enemyDamage * enemycrit);
 
-    let myDamage = Math.ceil(Math.random() *20);
-    let myCritDamage = Math.ceil(Math.random() *10);
-    let mycrit = myCritDamage > 7 ? 1.5 : 1;
-    myDamage = Math.ceil( myDamage * mycrit);
-
-    enemyHp -= myDamage;
-    playerHp -= enemyDamage;
-
-    alert(`
+const displayGameState = (enemyDamage, playerHp, myDamage, enemyHp) =>{
+     alert(`
         нам нанесли ${enemyDamage}
          у нас осталось ${playerHp} 
          Мы нанесли ${myDamage}
-         у него осталсь ${enemyHp} `)
+         у него осталсь ${enemyHp} 
+         `);
 }
-if(playerHp > 0 && enemyHp < 0) {
-    alert('Вы победили')
-} else if(enemyHp > 0 && playerHp < 0) {
-    alert('Вы проиграли')
-} else {
-    alert('ничья')
+const calculateDamage = () =>{
+    let damage = Math.ceil(Math.random() * MAX_DAMAGE);
+    return Math.random() < CRIT_CHANCE ? Math.ceil(damage * CRIT_MULTIPLICATOR)  : damage;
+} 
+
+const gameStart = () =>{
+
+    let playerHp = HP;
+    let enemyHp = HP;
+
+    while(playerHp > 0 && enemyHp > 0) {
+        let shoot = confirm('стрелять?')
+        if(!shoot ) {
+        break
+        }
+        let enemyDamage =  calculateDamage();
+        let myDamage = calculateDamage();
+
+        enemyHp -= myDamage;
+        playerHp -= enemyDamage;
+
+        displayGameState(enemyDamage, playerHp, myDamage, enemyHp)
+    }
+
+    switch(true) {
+        case playerHp > 0 && enemyHp < 0 :
+            alert('Вы победили');
+            break;
+        case enemyHp > 0 && playerHp < 0 : 
+            alert('Вы проиграли');
+            break;
+        default :
+            alert('ничья');
+    }
 }
+
+gameStart();
