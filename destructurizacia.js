9f278986c6da2309b16a056aa0260984903f7a73
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-07-01 11:38:59 +0300

    task

--- destructurizacia.js
+++ destructurizacia.js
@@ -5,9 +5,23 @@ const bodyParts = ["глаз", "нос", "череп","жопа", "лапки"];
 // const ass = bodyParts[3];
 // const paws = bodyParts[4]; 
 
-// const [eye, nose, sculk, ass, paws] = bodyParts
+// const [eye, nose, sculk, ass, paws] = bodyParts;
+// const [eye, nose] = bodyParts;
+// console.log(eye, nose);
+
 // const [eye, nose, , ass, paws] = bodyParts
 // const [eye, nose, ...array] = bodyParts
 const [, , ...array ] = bodyParts;
 // console.log(eye, nose);
-console.log(array);
+// console.log(array);
+const getNames = () =>{
+    return  ['Ратмир', 'Дима'];
+};
+
+const [ratmir, dima] = getNames();
+console.log(ratmir, dima);
+
+const sum = (...numbers) =>{
+    console.log(numbers); 
+}
+console.log(sum(52, 28, 458, 83, 54))
