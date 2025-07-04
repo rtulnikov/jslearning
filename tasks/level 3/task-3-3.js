dabfe3e0e7933f7dc913280274691aa4d59d21be
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-07-04 18:29:17 +0300

    task-3-3

--- tasks/level 3/task-3-3.js
+++ tasks/level 3/task-3-3.js
@@ -1,15 +1,17 @@
-const bodyParts = ["глаз", "нос", "череп","жопа"];
-const adjectives = ["вонючая", "унылая", "дурацкая", "тупая", "афигевшая"];
-const words = ["муха", "выдра", "дубина", "мартышка", "крыса"];
-
-const sayTeaser = (arrayInFunc) =>{
-let randomElement = arrayInFunc[Math.floor(Math.random() * bodyParts.length)];
+const getRandomeElement = (array) =>{
+let randomElement = array[Math.floor(Math.random() * array.length)];
     return randomElement;
+}
+const sayTeaser = () =>{
+    const bodyParts = ["глаз", "нос", "череп","жопа"];
+    const adjectives = ["вонючая", "унылая", "дурацкая", "тупая", "афигевшая"];
+    const words = ["муха", "выдра", "дубина", "мартышка", "крыса"];
+
+    return 'У тебя ' + getRandomeElement(bodyParts)+ ', ' + 'как ' + getRandomeElement(adjectives) + ' ' + getRandomeElement(words);
+
 }
 
 
-let vyvod = 'У тебя ' + sayTeaser(bodyParts)+ ', ' + ' как ' + sayTeaser(adjectives) + ' ' + sayTeaser(words);
 
-
-console.log(vyvod)
+console.log(sayTeaser())
 
