98e78e6a273d21287093c58020180219b6a0154a
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-07-29 18:24:34 +0300

    мясо, мясорубка и фарш

--- destructurizacia.js
+++ destructurizacia.js
@@ -7,6 +7,7 @@ const bodyParts = ["глаз", "нос", "череп","жопа", "лапки"];
 
 // const [eye, nose, sculk, ass, paws] = bodyParts;
 // const [eye, nose] = bodyParts;
+
 // console.log(eye, nose);
 
 // const [eye, nose, , ass, paws] = bodyParts
@@ -15,6 +16,16 @@ const [, , ...array ] = bodyParts;
 // console.log(eye, nose);
 console.log(array);
 
+const destr = () => {
+    return bodyParts;
+}
+
+
+destr()
+destr()
+const [eye, , sculk, ...massiv] = destr();
+console.log(eye,  sculk )
+console.log(massiv)
 const sum = (...numbers) =>{
     let pustota = 0;
     for(let i = 0; i < numbers.length; i++) {
--- practice/buy-candies.js
+++ practice/buy-candies.js
@@ -1,3 +1,40 @@
 const CHOCOLATE_PRICE = 10;
 const CARAMEL_PRICE = 3;
-const MAX_CHOCOLATES = 5;
+const maxCount = 5;
+
+const buyCandies = (money) => {
+    let chocolateCount = Math.floor(money / CHOCOLATE_PRICE);
+    if(chocolateCount > maxCount) chocolateCount = maxCount;
+
+    let changeOfChocolate = money - CHOCOLATE_PRICE * chocolateCount;
+
+
+    let caramelCount = Math.floor(changeOfChocolate / CARAMEL_PRICE);
+    let change = changeOfChocolate - caramelCount * CARAMEL_PRICE;
+    return [chocolateCount,  caramelCount, change];
+}
+//рефакторинг
+const calculateCandies = (money, price, maxCount) => {
+    if(money === undefined) {
+        console.log('Где деньги Матроскин')
+        return false;
+    }
+    let count = Math.floor(money / price);
+    if(count > maxCount && maxCount !== undefined) count = maxCount;
+    let change = money - price * count;
+    return [count, change]
+}
+// const [eye, nose] = bodyParts;
+
+const buyallCandies = (money) => {
+    let [chocolateCount,afterChocolate] = calculateCandies(money, CHOCOLATE_PRICE, maxCount);
+    let [caramelCount,change] = calculateCandies(afterChocolate, CARAMEL_PRICE);
+
+    return [chocolateCount,  caramelCount, change];
+}
+const result = buyallCandies(60);
+
+
+console.log(`${result[0]} шоколадок, ${result[1]} карамели, ${result[2]} р. сдача`);
+
+
