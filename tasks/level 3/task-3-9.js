aa90570c1bf79cffe49b2639b1e622e86f872507
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-07-30 17:41:15 +0300

    new task

--- tasks/level 3/task-3-9.js
+++ tasks/level 3/task-3-9.js
@@ -1,3 +1,15 @@
+3.9 /*
+Есть функция, которая возвращает текущую дату в виде массива :
+function getDateParts() {
+  const date = new Date(); // Текущая дата
+  return [date.getDate(), date.getMonth() + 1, date.getFullYear()];
+}
+Нужно через деструктуризацию получить данные из функции и вывести в консоль:
+Сегодняшняя дата: 30.07.2025
+Чтобы все работало нужно функцию getDateParts скопировать в файл с решением задачи.
+EASY
+*/
+
 function getDateParts() {
   const date = new Date(); // Текущая дата
   return [date.getDate(), date.getMonth() + 1, date.getFullYear()];
