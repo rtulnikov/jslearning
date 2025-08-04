1cc71bff3665df2d79c9b94b4b7c86abe64df3b8
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-08-04 17:32:06 +0300

    task-3-13.js

--- tasks/level 3/task-3-13.js
+++ tasks/level 3/task-3-13.js
@@ -2,8 +2,8 @@ const reverse = (str) => {
     let reverse = ''
     for(let i = str.length -1; i >= 0; i--) {
     reverse += str[i];
+}
     return reverse;
 }
-}
-console.log(reverse('Дима'));
+console.log(reverse('лош'));
 
