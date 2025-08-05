0f4689d22931367548a752d31b20d20e1f01d3a8
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-08-05 20:59:22 +0300

    some corrections

--- tasks/level 3/task-3-12.js
+++ tasks/level 3/task-3-12.js
@@ -1,12 +1,12 @@
-const numbers = [-225555, 45055550, 12];
+const numbers = [1, 45055550, -2];
 
 const min = (arr) => {
-    let minNumber = arr[0]; 
-        for(let i = 1; i < arr.length;  i++) { 
-            if(minNumber > arr[i]) minNumber = arr[i];   
-        }
+    let minNumber = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (minNumber > arr[i]) minNumber = arr[i];
+    }
 
     return minNumber;
-}
+};
 
-console.log(min(numbers))
+console.log(min(numbers));
--- tasks/level 3/task-3-13.js
+++ tasks/level 3/task-3-13.js
@@ -1,9 +1,8 @@
 const reverse = (str) => {
-    let reverse = ''
-    for(let i = str.length -1; i >= 0; i--) {
-    reverse += str[i];
-}
+    let reverse = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverse += str[i];
+    }
     return reverse;
-}
-console.log(reverse('Дима'));
-
+};
+console.log(reverse("Дима"));
