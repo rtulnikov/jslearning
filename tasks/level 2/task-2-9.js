297025dc155e5853374c77f277febbc398d267f2
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-09-13 07:05:47 +0300

    task-2-9.js

---
+++ tasks/level 2/task-2-9.js
@@ -0,0 +1,8 @@
+/*
+ЗАписать все элементы массива в переменные через десруктуризацию
+const array = ['Олени', 'папа какашка', 'ты балбес']
+SUPER SUPER PUPER PUPER EASY
+*/
+const array = ['Олени', 'папа какашка', 'ты балбес']
+const [oleni, papaKakashka, tiBalbec] = array;
+console.log(oleni,papaKakashka,tiBalbec)
