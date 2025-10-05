307025b50cf5a53d8a2a43a43c7277c11674633f
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-10-05 18:57:03 +0300

    task-4-11.js

---
+++ tasks/level 4/task-4-11.js
@@ -0,0 +1,35 @@
+// //1
+// const students = [
+//   { name: "Анна", age: 20 },
+//   { name: "Марк", age: 22 },
+//   { name: "Юля", age: 19 }
+// ]; 
+
+// const names = students.map( (elem) => {
+//     console.log(elem.name)
+// })
+//2
+// const user = {
+//   name: "Иван",
+//   age: 30,
+//   isAdmin: true
+// };
+
+
+// const length = Object.entries(user).length
+// console.log(length)
+
+//3
+// let config = {
+//   theme: "dark",
+//   language: undefined,
+//   debug: true,
+//   version: undefined
+// };
+
+// for(let key in config){
+//     if(config[key] == undefined) {
+//         delete config[key]
+//     }
+// }
+// console.log(config)
