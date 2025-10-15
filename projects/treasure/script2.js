88fe415113b7ab87a3d3745be41ae1880a73d0c6
Тульников <tulnikovratmir@gmail.com>
2025-10-15 09:55:46 +0300

    treasure

--- projects/treasure/script2.js
+++ projects/treasure/script2.js
@@ -11,48 +11,47 @@
 
 const maxWeight = 400;
 const maxHeight = 400;
-const map = document.querySelector(".picture")
-const message = document.querySelector(".message")
+const map = document.querySelector(".picture");
+const message = document.querySelector(".message");
 
 const treusure = {
     x: Math.floor(Math.random() * maxWeight),
     y: Math.floor(Math.random() * maxHeight),
-}
+};
 
 const getTreasure = () => {
     let img = document.createElement("img");
     img.src = "img/treasure.png";
     img.alt = "treusure";
     img.style.position = "absolute";
-    img.style.top = treusure.y - 35 + "px";
-    img.style.left = treusure.x - 38 + "px";
+    img.style.top = treusure.y - 18 + "px";
+    img.style.left = treusure.x - 15 + "px";
     map.append(img);
 };
 
-let click = 0;
-map.addEventListener("click", (event) => {
-    click++
+function handleClick(event) {
+    click++;
     const difX = event.offsetX - treusure.x;
     const difY = event.offsetY - treusure.y;
-    const distance = Math.floor(Math.sqrt(difX * difX + difY * difY))
-    console.log(distance)
+    const distance = Math.floor(Math.sqrt(difX * difX + difY * difY));
 
-        switch (true) {
-        case distance >= 200:
-            message.innerText = "ультра  холодно";
-            break
-        case distance >= 100:
-            message.innerText = "теплее";
-            break
-        case distance >= 50:
-            message.innerText = "горячо";
-            break
-        case distance >= 10:
-            message.innerText = "горим";
-            break
-        case distance >= 0:
-
-            message.innerText = "клад найден! Вы справились за" + click + "попыток";
-            getTreasure();
+    let clue = "";
+    if (distance >= 200) {
+        clue = "ультра  холодно";
+    } else if (distance >= 100) {
+        clue = "теплее";
+    } else if (distance >= 50){
+        clue = "горячо";
+    } else if (distance >= 10) {
+        clue = "горим";
+    }else   {
+        clue = "клад найден! Вы справились за " + click + " попыток";
+        getTreasure();
+        map.removeEventListener("click", handleClick);
     }
-})
+
+    message.innerText = clue;
+}
+
+let click = 0;
+map.addEventListener("click", handleClick);
