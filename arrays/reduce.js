cd5e0db32a65e136f82d9919a1b8023483744282
Тульников <tulnikovratmir@gmail.com>
2025-10-21 20:30:12 +0300

    reduce

--- arrays/reduce.js
+++ arrays/reduce.js
@@ -4,18 +4,41 @@ for (let elem of numbers) {
     sum += elem;
 }
 
-console.log(numbers.reduce( (acc, elem, index, aarray) =>{
-    console.log(acc, elem, index, aarray)
-    return acc + elem},
-    0 ));
-let avg = numbers.reduce( (acc, elem, index) => {
-    console.log(acc, elem, index)
-    if(index == 4){
-      return  (acc + elem) / numbers.length
+console.log(
+    numbers.reduce((acc, elem, index, aarray) => {
+        console.log(acc, elem, index, aarray);
+        return acc + elem;
+    }, 0)
+);
+let avg = numbers.reduce((acc, elem, index) => {
+    console.log(acc, elem, index);
+    if (index == 4) {
+        return (acc + elem) / numbers.length;
+    } else {
+        return acc + elem;
     }
-    else{
-       return acc + elem ; 
-    }
-    
-} )
-console.log(avg)
+});
+// console.log(avg)
+
+const words = ["JavaScript", "is", "poo"];
+console.log(words.join(" "));
+
+let result = "";
+for (let i = 0; i < words.length; i++) {
+    result += words[i] + " ";
+}
+
+const jh = words.reduce((acc, elem) => (acc += " " + elem));
+
+numbers = [45, 2, 65, 58, 10];
+acc  = 0;
+// for (let elem of numbers) {
+//     acc += elem
+// }
+// console.log(sum)
+const sumw = numbers.reduce((acc,elem) =>{
+    console.log(acc,elem)
+    acc += elem
+    return acc
+})
+console.log(sumw)
--- objects/object-this.js
+++ objects/object-this.js
@@ -2,52 +2,53 @@ const greet = function () {
     return "Привет, " + this.name;
 };
 
-
 const dima = {
     name: "дима",
     age: 5,
-    greet
+    greet,
 };
 const ratmir = {
     name: "ратмир",
     age: 11,
     greet: greet,
 };
-console.log(ratmir.greet())
-console.log(dima.greet());
+// console.log(ratmir.greet())
+// console.log(dima.greet());
 
 const userActions = {
-    run: function(){
-        console.log(this.name + ' бежит')
+    run: function () {
+        console.log(this.name + " бежит");
     },
-    walk(){
-        console.log(this.name + ' идет')
+    walk() {
+        console.log(this.name + " идет");
     },
-    
-}
+};
 const user = {
-    name: 'Вася',
-    role: 'user',
+    name: "Вася",
+    role: "user",
     run: userActions.run,
     walk: userActions.walk,
-}
+};
 const admin = {
-    name: 'Петя',
-    role: 'admin',
+    name: "Петя",
+    role: "admin",
     run: userActions.run,
     walk: userActions.walk,
-}
+};
 const moderator = {
-    name: 'Коля',
-    role: 'moderator',
+    name: "Коля",
+    role: "moderator",
     run: userActions.run,
     walk: userActions.walk,
-}
-admin.run()
+};
+admin.run();
+//Привязываем метод к обьекту admin
+const adminRun = userActions.run.bind(admin)
+adminRun()
+const userWalk = userActions.walk.bind(user)
+setTimeout(userWalk, 3000);
 
-setTimeout( () => user.run(),2000)
-
-function test(){
+function test() {
     user.run();
-    admin.walk()
+    admin.walk();
 }
