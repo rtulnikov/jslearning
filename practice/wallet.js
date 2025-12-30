cb2888f9579ca582f28157aaa3de5db0ef41b9a4
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2025-12-30 10:41:03 +0300

    ывафыа

--- practice/wallet.js
+++ practice/wallet.js
@@ -2,8 +2,12 @@ const wallet = {
     balance: 0,
     operations: [],
     increase(number, operation) {
-        this.operations.push({ reson: operation, sum: number });
-        return (this.balance += number);
+        if (number && operation) {
+            this.operations.push({ reson: operation, sum: number });
+            return (this.balance += number);
+        } else {
+            return;
+        }
     },
     reduce(number, operation) {
         if (this.balance < number) {
@@ -12,7 +16,7 @@ const wallet = {
                 sum: -number,
             });
             return false;
-        } 
+        }
         this.operations.push({ reson: operation, sum: -number });
         this.balance -= number;
         return true;
@@ -23,23 +27,23 @@ const wallet = {
     getBalance() {
         return this.balance;
     },
-    getAlert(){
-        return this.operations.filter( (obj) => obj.hasOwnProperty('alert') )
+    getAlert() {
+        return this.operations.filter((obj) => obj.hasOwnProperty("alert"));
     },
-    getAlert2(){
-        return this.operations.filter( (obj) =>{
-            return 'alert' in obj
-        })
+    getAlert2() {
+        return this.operations.filter((obj) => {
+            return "alert" in obj;
+        });
     },
 };
 
 wallet.increase(5156, "зарплата");
 console.log(wallet.operations);
 console.log(wallet.balance);
+wallet.balance -= wallet.balance;
 wallet.reduce(6450, "покупка кофеварки");
 wallet.reduce(6350, "покупка кофеварки");
 console.log(wallet.operations);
 console.log(wallet.balance);
 console.log(wallet.getOperations());
-console.log(wallet.getAlert2())
-console.log(this)
+console.log(wallet.getAlert2());
