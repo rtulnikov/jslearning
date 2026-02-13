f123307f727dee3aeb1863d277b89073338edb34
Виталий Тульников <vital2k9@gmail.com>
2026-02-13 19:41:52 +0300

    Приведение типов

--- functions/sort.js
+++ functions/sort.js
@@ -1,14 +1,14 @@
 const names = ['Дима', 'Вова', 'Вася', 'Леха'];
 const numbers = [3,1, 10, 15, 100];
-// numbers.sort( (a, b) => {
-//     if(a > b) {
-//         return 1;
-//     }else if(a < b) {
-//         return -1;
-//     }else {
-//         return 0;
-//     }
-// });
+numbers.sort( (a, b) => {
+    if(a > b) {
+        return 1;
+    }else if(a < b) {
+        return -1;
+    }else {
+        return 0;
+    }
+});
 
 // numbers.sort( (a, b) => {
 //     if(a > b) return 245258541;      
@@ -16,7 +16,19 @@ const numbers = [3,1, 10, 15, 100];
 //     if(a == b) return 0; 
 // });
 
-// numbers.sort( (a, b) => b - a );
+console.log(numbers.sort( (a, b) => a - b ));
 
 //filter(numbers, (number) => number % 2 != 0);
-console.log(names.sort());
+console.log(numbers.sort());
+const arr  = [
+[1, 2, 3],
+[1, 2],
+[1, 2, 3, 4, 5],
+[1],
+[1, 2, 3, 4],
+]
+// arr.sort( (a,b) => a.length - b.length)
+
+
+console.log([...arr].sort( (a, b) => a.length - b.length ))
+console.log(arr)
