6e51a4222bfbc1f1a5be341dccbc9c4f7515bce6
Tulnikov Ratmir <tulnikovratmir@gmail.com>
2026-04-25 17:47:52 +0300

    del

--- DOM/deligirovanie/deligirivanie.js
+++ DOM/deligirovanie/deligirivanie.js
@@ -1,3 +1,21 @@
 'use strict'
+let list = document.querySelector("#list")
+const tanks = ["T-34", "Panzer IV", "Sherman", "IS-2", "M24 Chaffee"];
 
+list.addEventListener("click", (e) =>{
+    let li = e.target.closest("li")
+    if(!li) return;
+    let activeLi = list.querySelector(".red")
+    if(activeLi) {
+        activeLi.classList.remove("red")
+    }
+    li.classList.add("red")
+})
 
+for(let el of tanks){
+    let li = document.createElement("li")
+
+    
+    li.textContent = el
+    list.append(li)
+}
--- closure/var.js
+++ closure/var.js
@@ -1,8 +1,10 @@
 
 
 
-for (var i = 0; i < 3; i++) {
-    setTimeout(function() {
-        console.log(i);
-    }, 100);
-}
+    console.log(i)
+    for (var i = 0; i < 3; i++) {
+        setTimeout(function(){
+            console.log(i)
+        },0)
+    }
+    console.log(i)
